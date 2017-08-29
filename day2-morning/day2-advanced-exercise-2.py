37cb65f1ccb6a411a73f38bd48c9334700b2d60f
mehurlock <mhurlock@udel.edu>
2017-08-29 14:32:42 -0400

    Day 2 morning exercise update

---
+++ day2-morning/day2-advanced-exercise-2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+
+fh = open(sys.argv[1])
+count=0
+
+for line in fh:
+    if line.startswith ( "@"):
+        continue
+    else:
+        broken=line.split("\t")
+        if broken[2]=="2L":
+            if int(broken[3])>=10000 and int(broken[3])<20001:
+                count=count=count+1
+print count
+    
