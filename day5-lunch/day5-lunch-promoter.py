cc900360f72abbbc4565fde8d755a67da08a46ad
mehurlock <mhurlock@udel.edu>
2017-09-03 20:43:31 -0400

    Day 5 Lunch Exercise Revision

--- day5-lunch/day5-lunch-promoter.py
+++ day5-lunch/day5-lunch-promoter.py
@@ -2,8 +2,6 @@
 
 """
 Usage ./day5-lunch-promoter.py <tab_file> <ctab_file> > <output_file_name>
-
--Plot timecourse of FBtr0331261 for females
 """
 
 import sys
@@ -38,33 +36,3 @@ model = sm.OLS(avgtrue,fpkmstrue)
 results = model.fit()
 print (results.summary())
     
-"""    avg.append(float(fields[4]))
-    
-fpkms=[]
-df = pd.read_csv(sys.argv[2], sep = "\t")
-fpkms = df["FPKM"].values.tolist()
-
-model = sm.OLS(avg,fpkms)
-results = model.fit()
-print (results.summary())"""
-
-"""fpkms=[]
-df = pd.read_csv(sys.argv[1], sep = "\t")
-fpkms = df["FPKM"].values.tolist()
-
-print fpkms
-
-histone_mean=[]
-fh=open(sys.argv[2])
-
-for line in fh:
-    fields = line.rstrip("\r\n").split()
-    histone_mean.append(fields[4])
-    
-print len(histone_mean)
-print len(fpkms)
-
-model = sm.OLS(histone_mean, exog = fpkms)
-results = model.predict()
-print results"""
-
--- day5-lunch/day5-lunch.py
+++ day5-lunch/day5-lunch.py
@@ -2,8 +2,6 @@
 
 """
 Usage ./day5-lunch.py <ctab> <output_name.bed>
-
--Plot timecourse of FBtr0331261 for females
 """
 
 import sys
@@ -30,38 +28,4 @@ for line in f:
             new_final = int(final) -500
             if new_final <0:
                 new_final = 0
-            print rows[1] + "\t %d \t %d" % (new_final, new_start) + "\t" + rows[5]
-        
-    """else:
-        start -=501
-        end += 499
-    for i in range(0, len(rows)-1):
-        final_rows=[strand, start, end]
-    return final_rows
-
-print final_rows"""
-
-
-"""coi = ["chr", "promoter_start", "promoter_end", "t_name"]
-
-coi_pos = df["strand"]=="+"
-coi_neg = df["strand"]=="-"
-
-df.loc[coi_pos, "promoter_start"] = round(df[coi_pos]["start"] - 499, 1)
-df.loc[coi_pos, "promoter_end"] = df[coi_pos]["start"]+501
-df.loc[coi_neg, "promoter_start"] = df[coi_neg]["end"]-501
-df.loc[coi_neg, "promoter_end"] = df[coi_neg]["end"]+499
-
-for strand in df["strand"][coi_pos]:
-    df["promoter_start"] = df["start"]-499
-    df["promoter_end"] = df["start"]+501
-    
-for stand in df["strand"][coi_neg]:
-    df["promoter_start"] = df["end"]-501
-    df["promoter_end"] = df["end"]+499
-    
-num=df._get_numeric_data()
-num[num<0]=0
-
-print df[coi].sort_values(columns="t_name")
-#df[coi].to_csv(sys.argv[2], sep = "\t", index=False, header=False)"""
+            print rows[1] + "\t %d \t %d" % (new_final, new_start) + "\t" + rows[5]x
